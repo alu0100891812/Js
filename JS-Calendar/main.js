dd8e323bc306c71155038daed591761d06e16516
Rafael <alu0100891812@ull.edu.es>
2017-10-10 09:23:09 +0000

    Calendario terminado

--- JS-Calendar/main.js
+++ JS-Calendar/main.js
@@ -6,9 +6,23 @@ var diasMes = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,
 var mesesAno = [31,28,31,30,31,30,31,31,30,31,30,31];
 var mesesAnoBisiesto = [31,29,31,30,31,30,31,31,30,31,30,31];
 var mesesNombre = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var mesesNombreEsp = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
 
 //Pintar HTML
-document.getElementById('calendar').innerHTML = '<div id="calendarEncabezado"><button id="calenarToday" onclick="IrHoy()">Hoy</button><button id="calendarMonthPrev" onclick="uptMonth(-1)">Prev</button><span id="calendarMonthName"></span><button id="calendarMonthNext" onclick="uptMonth(1)">Next</button></div><div class="semanas"><div class="titsem"><div class="day">Lunes</div><div class="day">Martes</div><div class="day">Miercoles</div><div class="day">Jueves</div><div class="day">Viernes</div><div class="day">Sabado</div><div class="day">Domingo</div></div><div id="sem0" class="sem"><div class="day" id="d01"></div><div class="day" id="d02"></div><div class="day" id="d03"></div><div class="day" id="d04"></div><div class="day" id="d05"></div><div class="day" id="d06"></div><div class="day" id="d07"></div></div><div id="sem1" class="sem"><div class="day" id="d08"></div><div class="day" id="d09"></div><div class="day" id="d10"></div><div class="day" dia="d11"></div><div class="day" id="d12"></div><div class="day" id="d13"></div><div class="day" id="d14"></div></div><div id="sem2" class="sem"><div class="day" id="d15"></div><div class="day" id="d16"></div><div class="day" id="d17"></div><div class="day" id="d18"></div><div class="day" id="d19"></div><div class="day" id="d20"></div><div class="day" id="d21"></div></div><div id="sem3" class="sem"><div class="day" id="d22"></div><div class="day" id="d23"></div><div class="day" id="d24"></div><div class="day" id="d25"></div><div class="day" id="d26"></div><div class="day" id="d27"></div><div class="day" id="d28"></div></div><div id="sem4" class="sem"><div class="day" id="d29"></div><div class="day" id="d30"></div><div class="day" id="d31"></div><div class="day" id="d32"></div><div class="day" id="d33"></div><div class="day" id="d34"></div><div class="day" id="d35"></div></div><div id="sem5" class="sem"><div class="day" id="d36"></div><div class="day" id="d37"></div><div class="day" id="d38"></div><div class="day" id="d39"></div><div class="day" id="d40"></div><div class="day" id="d41"></div><div class="day" id="d42"></div></div></div>';
+var calendar = document.getElementById('calendar');
+calendar.innerHTML = '<div id="calendarEncabezado"><button id="calendarToday" onclick="IrHoy()">Hoy</button><button id="calendarMonthPrev" onclick="uptMonth(-1)">Anterior</button><div id="calendarMonthName"></div><button id="calendarMonthNext" onclick="uptMonth(1)">Siguiente</button></div><div class="semanas"><div class="titsem"><div class="day name">Lunes</div><div class="day name">Martes</div><div class="day name">Miercoles</div><div class="day name">Jueves</div><div class="day name">Viernes</div><div class="day name">Sabado</div><div class="day name">Domingo</div></div>';
+
+var day=1;
+for(var i=0; i<6; i++) {
+    var sem = document.createElement('div');
+    sem.id = "sem" + i;
+    sem.className = "sem";
+    calendar.appendChild(sem);
+    for (var j=0; j<7 ; j++, day++) {
+        sem.innerHTML += '<div class="day" id="d' + day + '"></div>';
+    }
+    calendar.innerHTML += '</div>';
+}
 
 //Rellenar  datos calendario
 dibujarCalendario(1);
@@ -35,7 +49,7 @@ function dibujarCalendario(dibujaHoy) {
         comienzo = 7 + comienzo;
     }
     //Relleno encabezado
-    document.getElementById('calendarMonthName').innerHTML = getEleVec(mesesNombre, mesNum);
+    document.getElementById('calendarMonthName').innerHTML = getEleVec(mesesNombreEsp, mesNum) + ' ' + ano;
 
     //Relleno de numeros del calendario
     var indice = 0 - diaSemanaNum;
@@ -43,6 +57,7 @@ function dibujarCalendario(dibujaHoy) {
     for(var j=0; j<6; j++){
         var NumSemana = 'sem' + j;
         var DiasSemana = document.getElementById(NumSemana);
+        if(j==5) var dy = [0,0,0,0,0,0,0];
         //Selecciona los dias
         for(var i=indice; i<(indice+7); i++){
             var dia = DiasSemana.childNodes[i+diaSemanaNum];
@@ -54,31 +69,58 @@ function dibujarCalendario(dibujaHoy) {
                     //Comprobacion de dias anteriores al mes
                     if((i+diaSemanaNum)<comienzo){
                         dia.innerHTML = (getEleVec(mesesAnoBisiesto, mesNum-1) - ((comienzo-1)-(i+diaSemanaNum)));
+                        dia.classList.add('disable');
+                        if (j==5) dy[i-indice] = 0;
                     }else{
                         dia.innerHTML = NumTemp;
+                        dia.classList.remove('disable');
+                        if (j==5) dy[i-indice] = 1;
                     }
                 }else {
                     //Comprobacion de dias siguientes al mes
                     if(NumTemp<=mesesAnoBisiesto[mesNum]){
                         dia.innerHTML = NumTemp;
+                        dia.classList.remove('disable');
+                        if (j==5) dy[i-indice] = 1;
                     }else{
                         dia.innerHTML = NumTemp%getEleVec(mesesAnoBisiesto, mesNum);
+                        dia.classList.add('disable');
+                        if (j==5) dy[i-indice] = 0;
                     }
                 }
             }else{
                 if(j==0){
                     if((i+diaSemanaNum)<comienzo){
                         dia.innerHTML = (getEleVec(mesesAno, mesNum-1) - ((comienzo-1)-(i+diaSemanaNum)));
+                        dia.classList.add('disable');
+                        if (j==5) dy[i-indice] = 0;
                     }else{
                         dia.innerHTML = NumTemp;
+                        dia.classList.remove('disable');
+                        if (j==5) dy[i-indice] = 1;
                     }
                 }else {
                     if(NumTemp<=mesesAno[mesNum]){
                         dia.innerHTML = NumTemp;
+                        dia.classList.remove('disable');
+                        if (j==5) dy[i-indice] = 1;
                     }else{
                         dia.innerHTML = NumTemp%getEleVec(mesesAno, mesNum);
+                        dia.classList.add('disable');
+                        if (j==5) dy[i-indice] = 0;
                     }
                 }
+            }
+        }
+        if(j==5) {
+            var dis = false;
+            dy.forEach(function(e){
+                if(e) dis=true;
+            });
+            if(!dis) {
+                DiasSemana.style.display = "none";
+            } else {
+                DiasSemana.style.display = "block";
             }
         }
     }
