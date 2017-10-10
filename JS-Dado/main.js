d71ac4f176458e5ce3068cf451b1a8cafdf8ac05
Rafael <alu0100891812@hotmail.com>
2017-10-10 07:35:14 +0100

    Dado terminado

--- JS-Dado/main.js
+++ JS-Dado/main.js
@@ -1,36 +1,52 @@
 var estadistica = [0,0,0,0,0,0];
+var showing = false;
 
 function lanzar() {
   var numero = Math.random();
   var dado = document.getElementById("dado");
-  if(numero<(1/6))
+
+  if(numero<(1/6)){
       numero = 1;
-  else if(numero<(2/6))
+  }else if(numero<(2/6)){
       numero = 2;
-  else if(numero<(3/6))
+  }else if(numero<(3/6)){
       numero = 3;
-  else if(numero<(4/6))
+  }else if(numero<(4/6)){
       numero = 4;
-  else if(numero<(5/6))
+  }else if(numero<(5/6)){
       numero = 5;
-  else if(numero<1)
+  }else if(numero<1){
       numero = 6;
+  }
   estadistica[numero-1]++;
-  dado.innerHTML = "El resultado es " + numero;
+  dado.style.backgroundImage = 'url("dice' + numero + '.png")';
+
+  actEst();
 }
 
 function mostrarEst() {
+  if(!showing) {
+    document.getElementById("bars").style.height = "210px";
+    document.getElementById("estadistica").style.height = "240px";
+    document.getElementById('showEst').innerHTML = "Ocultar estadistica";
+    showing = true;
+  }else{
+    document.getElementById("bars").style.height = "0px";
+    document.getElementById("estadistica").style.height = "80px";
+    document.getElementById('showEst').innerHTML = "Mostrar estadistica";
+    showing = false;
+  }
+
+  actEst();
+}
+
+function actEst() {
   var max = 0;
   for(var i=0; i<6; i++){
     if(estadistica[i]>max){
       max=estadistica[i];
     }
   }
-
-  document.getElementById("bars").style.height = "210px";
-  document.getElementById("estadistica").style.height = "295px";
-
-
 
   for(var j=0; j<6; j++){
     document.getElementById((j+1) + "bar").style.width = ((270/max)*estadistica[j])+ "px";
